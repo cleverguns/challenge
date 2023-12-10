3d6ab6cab55469187fe63972c0c84ea17b504cca
Carlo Gimps <41587193+cleverguns@users.noreply.github.com>
2023-12-10 12:29:08 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
 import './main.scss'
- 
+  
+((d) => {
+    "use strict";
+    let toggler = d.querySelector(".toggler"),
+      sidebar = d.querySelector(".side-bar"),
+      openSideBar = false;
+    toggler.addEventListener("click", () => {
+      if (!openSideBar) {
+        sidebar.style.right = "0px";
+        openSideBar = true;
+        return;
+      }
+      sidebar.style.right = "-300px";
+      openSideBar = false;
+    });
+  })(document);
